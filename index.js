9f8a4580d9fccd031adea05a91ef1e272c90ebd2
Unknown <03l54rd1n3@gmail.com>
2018-04-10 01:00:15 +0200

    Example markdown

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ var toFixed = function(n) {
 }
 
 const bereich = [30000, 120000];
+var counter = 0;
+var zeilen = [];
 for(var i = bereich[0]; i <= bereich[1]; i+=1000) {
     getGehalt(i).then(gehalt => {
         var bruttoJahr = toFixed(gehalt.bJahr);
@@ -48,7 +50,30 @@ for(var i = bereich[0]; i <= bereich[1]; i+=1000) {
         var nettoJahr = toFixed(gehalt.nJahr);
         var nettoMonat = toFixed(gehalt.nMonat);
         var s = `${bruttoMonat};${bruttoJahr};${nettoMonat};${nettoJahr}\n`;
-
+        zeilen.push([gehalt.bJahr, s]);
         fs.appendFile('gehaelter.csv', s);
+        counter += 1000;
+    }).then(() => {
+        if (counter == bereich[1] - bereich[0] + 1000) { // done
+
+            const exampleFile = 'EXAMPLE.md';
+            try {
+                fs.unlinkSync(exampleFile);
+            } catch (e){}
+
+            fs.appendFileSync(exampleFile, '| brutto Monat | brutto Jahr | netto Monat | netto Jahr |\n');
+            fs.appendFileSync(exampleFile, '| :-: | :-: | :-: | :-: |\n');
+
+            zeilen.sort(function (a, b) {
+                if (a[0] < b[0]) return -1;
+                if (a[0] > b[0]) return 1;
+                return 0;
+            });
+            var s = '';
+            for (var i = 0; i < zeilen.length; i++) {
+                s += zeilen[i][1].replace(/;/ig, ' | ');
+            }
+            fs.appendFileSync(exampleFile, s);
+        }
     });
 }
